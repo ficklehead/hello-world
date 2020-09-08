0876cc90bf40430c88ab55a8502fff29b3182ee9
ficklehead <70968259+ficklehead@users.noreply.github.com>
2020-09-08 21:16:05 +0300

    Add files via upload

---
+++ задание19.f90
@@ -0,0 +1,19 @@
+program task19
+
+   implicit none
+
+   real :: A, T, n
+   integer :: i
+  print *, 'Task 19'
+   print *, 'Enter amount of money'
+   read *,A
+   print *, 'Enter percent per year'
+   read *,T
+   print *, 'Enter amount of years'
+   read *,n
+do i=1,n
+A=A+A*(T/100)
+ end do
+write(*,"(F8.2)") A
+   pause
+end program task19
